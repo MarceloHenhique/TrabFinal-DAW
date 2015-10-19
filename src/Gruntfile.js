a0115af8ca64ce48404bd5bc24c2bbdc7fd48604
zEdmundo <edmundo-rodrigues@hotmail.com.br>
2015-10-19 10:52:14 -0200

    Add grunt task

---
+++ src/Gruntfile.js
@@ -0,0 +1,58 @@
+"use strict";
+
+module.exports = function( grunt ) {
+
+	// Load all tasks
+	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+	grunt.initConfig({
+
+		// Watch
+		watch: {
+			css: {
+				files: [ '../assets/sass/**/*.scss' ],
+				tasks: [ 'compass' ]
+			},
+			js: {
+				files: [
+					'../assets/vendor/js/*.js',
+					'../assets/js/**/**/*.js',
+					'!../assets/vendor/js/angular.min.js'
+				],
+				tasks: [ 'uglify' ]
+			}
+		},
+
+		// Compile scss
+		compass: {
+			dist: {
+				options: {
+					force: true,
+					config: 'config.rb',
+					outputStyle: 'compressed'
+				}
+			}
+		},
+
+		// Concat and minify javascripts
+		uglify: {
+			options: {
+				mangle: false
+			},
+			dist: {
+				files: {
+					'../build/js/app.min.js': [
+						'../assets/vendor/js/*.js',
+						'../assets/js/**/**/*.js',
+						'!../assets/vendor/js/angular.min.js'
+					]
+				}
+			}
+		},
+
+	});
+
+	// registrando tarefa default
+	grunt.registerTask( 'default', [ 'watch' ] );
+
+};
