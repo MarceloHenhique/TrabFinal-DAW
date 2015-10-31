7768e950abf4bea8a1f500add5ea63a15c97e428
zEdmundo <edmundo-rodrigues@hotmail.com.br>
2015-10-31 18:59:24 -0200

    Edit query for getting questions

--- index.php
+++ index.php
@@ -18,20 +18,37 @@ $app->get("/", function () use ($app) {
 	get_footer();
 });
 
-$app->get("/question/", function () {
+$app->get("/question/", function () use ($app) {
 
 	$con = mysqli_connect("localhost", "root", "", "enem");
 
-	$query = mysqli_query($con, "SELECT * FROM myTable LIMIT 0,3");
+	$query = mysqli_query($con, "SELECT questions.* FROM exams
+		INNER JOIN exams_has_questions ON exams.id = exams_has_questions.exams_id
+		INNER JOIN questions ON exams_has_questions.questions_id = questions.id
+		WHERE exams.id = 1;");
 
 	$questions = array();
 
-	while ($row = mysqli_fetch_object($query))
-		$questions[] = $row;
+	while ($row = mysqli_fetch_object($query)) {
+		foreach ($row as $key => $value)
+			if (is_string($value))
+				$row->$key = utf8_encode($value);
 
+		$questions[] = $row;
+	}
+
+	sleep(1);
 	echo json_encode($questions);
 
 })->name("hello");
+
+$app->post("/results/", function () use ($app) {
+
+	$con = mysqli_connect("localhost", "root", "", "enem");
+
+	$query = mysqli_query("INSERT INTO results ()"); // should be continued
+
+});
 
 $app->get("/.*", function () use ($app) {
 	try {
