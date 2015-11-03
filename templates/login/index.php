5dbefffe345686605b681afb33a87caaaf6d2d22
Raul Julian <Raul Julian>
2015-11-03 20:54:48 -0200

    http login alredy

--- templates/login/index.php
+++ templates/login/index.php
@@ -1,7 +1,9 @@
 <div ng-controller="loginController as fCtrl">
   <form novalidate ng-submit="fCtrl.update(user)">
-    Login: <input type="text" ng-model="user.email" /><br />
+    Login: <input type="text" ng-model="user.login" /><br />
     Password <input type="password" ng-model="user.pssw" /><br />
     <button type="submit" ng-click="fCtrl.log(user)" />Logar</button>
+
+    <h1> {{ result }} </h1> 
   </form>
 </div>
